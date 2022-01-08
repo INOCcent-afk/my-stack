ca8b85f4c9e7384a040bff67d5d0c6d77b72bedd
Dave Inoc <michael828inoc@gmail.com>
2022-01-08 17:14:15 +0800

    graphql setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,33 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
+const main = async () => {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+    }),
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground({
+        // options
+      }),
+    ],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({
+    app,
+  });
+
+  app.listen(4000, () => {
+    console.log(`Server start on localhost: ${4000}`);
+  });
+};
+
+main();
